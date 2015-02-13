6a181bd9bdc9dfcf0f62aee2a9d37791fb056d16
jstop <jstein@clypd.com>
2015-02-13 13:45:12 -0500

    seed delete

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,14 @@ Footwork.create({first: 'jared', last: 'stein', score: 3})
 Footwork.create({first: 'larry', last: 'david', score: 2})
 Footwork.create({first: 'david', last: 'stein', score: 28})
 Footwork.create({first: 'nancy', last: 'stein', score: 5})
+Footwork.delete_all
 
 Cp3.create({first: 'josh', last: 'stein', minutes: 0, seconds: 8})
 Cp3.create({first: 'jared', last: 'stein', minutes: 2, seconds: 3})
 Cp3.create({first: 'larry', last: 'david', minutes: 1, seconds: 2})
 Cp3.create({first: 'david', last: 'stein', minutes: 2, seconds: 28})
 Cp3.create({first: 'nancy', last: 'stein', minutes: 1, seconds: 5})
+Cp3.delete_all
 
 Melo.create({first: 'josh', last: 'stein', minutes: 0, seconds: 8})
 Melo.create({first: 'josh', last: 'stein', minutes: 0, seconds: 8})
@@ -24,6 +26,7 @@ Melo.create({first: 'jared', last: 'stein', minutes: 2, seconds: 3})
 Melo.create({first: 'larry', last: 'david', minutes: 1, seconds: 2})
 Melo.create({first: 'david', last: 'stein', minutes: 2, seconds: 28})
 Melo.create({first: 'nancy', last: 'stein', minutes: 1, seconds: 5})
+Melo.delete_all
 
 #Control.create({first: 'jared', last: 'stein', minutes: 2,  seconds: 3})
 #Control.create({first: 'larry', last: 'david', minutes: 1,  seconds: 2})
@@ -35,3 +38,4 @@ Vertical.create({first: 'jared', last: 'stein', score: 3})
 Vertical.create({first: 'larry', last: 'david', score: 2})
 Vertical.create({first: 'david', last: 'stein', score: 28})
 Vertical.create({first: 'nancy', last: 'stein', score: 5})
+Vertical.delete_all
