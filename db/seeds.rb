20b9c182e85760c8fe5d96fbb96d7e90df95e7f8
Joshua Stein <josh.stein107@gmail.com>
2015-04-10 12:21:17 -0400

    use seed to delete

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Control.delete_all
+Footwork.delete_all
+Cp3.delete_all
+Melo.delete_all
+Vertical.delete_all
+
 Footwork.create({first: 'josh', last: 'stein', score: 8})
 Footwork.create({first: 'jared', last: 'stein', score: 3})
 Footwork.create({first: 'larry', last: 'david', score: 2})
 Footwork.create({first: 'david', last: 'stein', score: 28})
 Footwork.create({first: 'nancy', last: 'stein', score: 5})
-Footwork.delete_all
 
 Cp3.create({first: 'josh', last: 'stein', minutes: 0, seconds: 8})
 Cp3.create({first: 'jared', last: 'stein', minutes: 2, seconds: 3})
 Cp3.create({first: 'larry', last: 'david', minutes: 1, seconds: 2})
 Cp3.create({first: 'david', last: 'stein', minutes: 2, seconds: 28})
 Cp3.create({first: 'nancy', last: 'stein', minutes: 1, seconds: 5})
-Cp3.delete_all
 
 Melo.create({first: 'josh', last: 'stein', minutes: 0, seconds: 8})
 Melo.create({first: 'josh', last: 'stein', minutes: 0, seconds: 8})
@@ -26,16 +30,14 @@ Melo.create({first: 'jared', last: 'stein', minutes: 2, seconds: 3})
 Melo.create({first: 'larry', last: 'david', minutes: 1, seconds: 2})
 Melo.create({first: 'david', last: 'stein', minutes: 2, seconds: 28})
 Melo.create({first: 'nancy', last: 'stein', minutes: 1, seconds: 5})
-Melo.delete_all
 
-#Control.create({first: 'jared', last: 'stein', minutes: 2,  seconds: 3})
-#Control.create({first: 'larry', last: 'david', minutes: 1,  seconds: 2})
-#Control.create({first: 'david', last: 'stein', minutes: 2,  seconds: 28})
-#Control.create({first: 'nancy', last: 'stein', minutes: 1, seconds: 5})
+Control.create({first: 'jared', last: 'stein', minutes: 2,  seconds: 3})
+Control.create({first: 'larry', last: 'david', minutes: 1,  seconds: 2})
+Control.create({first: 'david', last: 'stein', minutes: 2,  seconds: 28})
+Control.create({first: 'nancy', last: 'stein', minutes: 1, seconds: 5})
 
 Vertical.create({first: 'josh', last: 'stein', score: 8})
 Vertical.create({first: 'jared', last: 'stein', score: 3})
 Vertical.create({first: 'larry', last: 'david', score: 2})
 Vertical.create({first: 'david', last: 'stein', score: 28})
-Vertical.create({first: 'nancy', last: 'stein', score: 5})
-Vertical.delete_all
+Vertical.create({first: 'nancy', last: 'stein', score: 7})
